0b8d4dead17e9e92c24c9a2888ea046911b6df03
FlorianLabranche <florian.labranche@gmail.com>
2018-03-13 13:58:53 +0100

    Custom route with header and footer

--- react-boilerplate/app/components/CustomRoute/index.js
+++ react-boilerplate/app/components/CustomRoute/index.js
@@ -5,17 +5,26 @@
 */
 
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+import { Container } from 'semantic-ui-react'
+import { height } from 'window-size';
 // import styled from 'styled-components';
 
 
 class CustomRoute extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const {path, component, withHeader } = this.props;
+    const { path, component, withHeader, exact } = this.props;
+    console.log(path)
+    console.log(component)
+    console.log(withHeader)
     return (
       <div>
         {withHeader && <Header />}
-        <Route exact path={path} component={component} />
+        <Container text style={{ marginTop: '7em', minHeight: '100vh' }}>
+          <Route exact={exact} path={path} component={component} />
+        </Container>
         <Footer />
       </div>
     );
